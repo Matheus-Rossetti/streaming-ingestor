282de922092932918d573e13189682abd3da3b26
Matheus <matheuzdreher@gmail.com>
2025-04-08 13:29:47 -0300

    enhance uploadHandler to validate file type and log file details

--- main.go
+++ main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"image"
+	"image/png"
 	"net/http"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
-	_, header, err := r.FormFile("file")
+	file, header, err := r.FormFile("file")
 	if err != nil {
 		http.Error(w, "Erro: "+err.Error(), http.StatusBadRequest)
 		return
 	}
+
+	if header.Header.Get("Content-Type") != "video/mp4" {
+	}
+
 	fmt.Println(header.Filename)
+	fmt.Println(header.Size)
+	fmt.Println(header.Header.Get("Content-Type"))
+
 }
 
 func main() {
